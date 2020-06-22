80592b4aae9e6b17503e3533643c4cf3e0bbab4c
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-22 17:57:38 +0200

    removed setters from entities

--- src/main/java/com/flairstech/workshop/Country.java
+++ src/main/java/com/flairstech/workshop/Country.java
@@ -28,47 +28,23 @@ public class Country {
         return capital;
     }
 
-    public void setCapital(int capital) {
-        this.capital = capital;
-    }
-
     public String getCode() {
         return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getContinent() {
         return continent;
-    }
-
-    public void setContinent(String continent) {
-        this.continent = continent;
     }
 
     public int getPopulation() {
         return population;
     }
 
-    public void setPopulation(int population) {
-        this.population = population;
-    }
-
     public int getLifeExpectancy() {
         return lifeExpectancy;
-    }
-
-    public void setLifeExpectancy(int lifeExpectancy) {
-        this.lifeExpectancy = lifeExpectancy;
     }
 }
--- src/main/java/com/flairstech/workshop/CountryLanguage.java
+++ src/main/java/com/flairstech/workshop/CountryLanguage.java
@@ -16,15 +16,7 @@ public class CountryLanguage {
         return countryCode;
     }
 
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
     public String getLanguage() {
         return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
     }
 }
